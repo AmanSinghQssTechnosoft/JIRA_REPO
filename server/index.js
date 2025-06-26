176bca20b89d59eb5689df42384326801d1d76bf
Aman Singh <aman.singh@qsstechnosoft.com>
2025-06-26 15:55:21 +0530

    notification-api-added

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const app = express();
-
+//schedule for remainder
+const reminderScheduler = require("./remainder/scheduler");
+// Importing routers
 const userRouter=require("./route/userRoutes");
 const taskRouter=require("./route/taskManagerRoutes");
 const mailRouter=require("./route/mailRoute");
 const authRoutes=require("./route/passwordRoutes");
 const googleAuth=require("./route/googleAuthRoute");
 const PdfRouter=require("./route/pdfRoute");
+const remainderRouter=require("./route/remainderRoute");
+const notificationRouter=require("./route/notificationRoute");
 app.use(cors());
 app.use(express.json());
 
@@ -21,6 +25,9 @@ app.use("/mail",mailRouter);
 app.use("/api", authRoutes);
 app.use("/googleauth",googleAuth);
 app.use("/pdf",PdfRouter);
+app.use("/remainder",remainderRouter);
+app.use("/notification",notificationRouter);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
